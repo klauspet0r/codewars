5332a6ec037151d396b76ddc7852c89957adeed3
Michal Zajac <michal.zajac@vw-fc.com>
2023-08-15 21:49:05 -0700

    first approach to the The Supermarket Queue kata
    doesn't return the right value

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
-function topThreeWords(text) {
-    // Remove non-alphanumeric characters except for apostrophes
-    const cleanString = text.replace(/[^A-Za-z']/g, ' ');
-  
-    // Convert to lowercase and split into words
-    const words = cleanString.toLowerCase().split(/\s+/);
-  
-    const wordCountMap = new Map();
-  
-    // Count occurrences of each word
-    for (const word of words) {
-      if (word.length > 0 && word !== '\'') {
-        if (wordCountMap.has(word)) {
-          wordCountMap.set(word, wordCountMap.get(word) + 1);
-        } else {
-          wordCountMap.set(word, 1);
-        }
-      }
-    }
-  
-    // Sort words by their occurrence count
-    const sortedWords = [...wordCountMap.entries()].sort((a, b) => b[1] - a[1]);
-  
-    // Get the top three words
-    const topThreeWords = sortedWords.slice(0, 3).map(entry => entry[0]);
-  
-    return topThreeWords;
+function queueTime(customers, numTills) {
+ 
+  const customersPerTill = Math.ceil(customers.length / numTills);
+  const tills = [];
+  let max_numbers = [];
+  let max_number = 0;
+  let initialValue = 0;
+
+  for (let i = 0; i < numTills; i++) {
+    tills.push([]);
+  }
+  for (let i = 0; i < customers.length; i++) {
+    const currentTillIndex = i % numTills;
+    tills[currentTillIndex].push(customers[i]);
   }
 
-let str =`In a village of La Mancha, the name of which I have no desire to call to
-mind, there lived not long since one of those gentlemen that keep a lance
-in the lance-rack, an old buckler, a lean hack, and a greyhound for
-coursing. An olla of rather more beef than mutton, a salad on most
-nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
-on Sundays, made away with three-quarters of his income.`;
- 
-console.log(topThreeWords(str));
+  tills.forEach(till => {max_numbers.push(till.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue));  
+    
+  });
 
+  max_numbers.forEach(element => { element > max_number ? max_number = element : max_number = max_number});
+  return max_number;
+}
+
+
+console.log(queueTime([10,2,3,3], 2) == 10 ? 'Passed':'Failed');
