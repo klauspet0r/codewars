454a2ac4e996c14b5589a1be4cfab773d03b4025
Michal Zajac <michal.zajac@vw-fc.com>
2023-08-16 22:31:33 -0700

    Solution to The Supermarket Queue
    https://www.codewars.com/kata/57b06f90e298a7b53d000a86/solutions/javascript

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 function queueTime(customers, numTills) {
- 
-  const customersPerTill = Math.ceil(customers.length / numTills);
-  const tills = [];
-  let max_numbers = [];
-  let max_number = 0;
-  let initialValue = 0;
+  const tills = new Array(numTills).fill(0);
+  let currentTime = 0;
 
-  for (let i = 0; i < numTills; i++) {
-    tills.push([]);
+  while (customers.length > 0) {
+    for (let i = 0; i < numTills; i++) {
+      if (tills[i] <= currentTime && customers.length > 0) {
+        const checkoutTime = customers.shift();
+        tills[i] = currentTime + checkoutTime;
+      }
+    }
+    currentTime++;
   }
-  for (let i = 0; i < customers.length; i++) {
-    const currentTillIndex = i % numTills;
-    tills[currentTillIndex].push(customers[i]);
-  }
+  let max_time = 0;
 
-  tills.forEach(till => {max_numbers.push(till.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue));  
-    
-  });
+  tills.forEach( element => {if (element > max_time) max_time = element});
 
-  max_numbers.forEach(element => { element > max_number ? max_number = element : max_number = max_number});
-  return max_number;
+  return max_time;
 }
-
 
 console.log(queueTime([10,2,3,3], 2) == 10 ? 'Passed':'Failed');
