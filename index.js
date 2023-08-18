0adcbddcd050a611eb974b94ad267672ff70179d
Michal Zajac <michal.zajac@vw-fc.com>
2023-08-18 09:58:48 -0700

    Solution with memoization

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const memo = {};
+
 function fibonacci(n) {
-  if(n==0 || n == 1)
-      return n;
-  return fibonacci(n-1) + fibonacci(n-2);
+  if (n <= 1) {
+    return n;
+  }
+
+  // Check if the result is already memoized
+  if (memo[n] !== undefined) {
+    return memo[n];
+  }
+
+  // Calculate and memoize the result
+  memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+  return memo[n];
 }
 
 const n = 10;
