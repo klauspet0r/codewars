96b8b3dec8819c7f955b5cc5c252e86fb34764c5
Michal Zajac <michal.zajac@vw-fc.com>
2023-08-17 21:36:30 -0700

    Solution to The Hashtag Generator

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 function generateHashtag (str) {
-  if (str. trim() === '') {
+  if (str.trim() === '') {
     return false; // Return false for empty strings
   }
 
-  // Split camel case string into individual words
-  const words = str.split(/(?=[A-Z])/);
+  // Split input string into individual words
+  const words = str.split(/\s+/);
 
   // Remove special characters and join words with camel case
   const cleanedWords = words.map(word => word.replace(/[^a-zA-Z0-9]/g, ''));
-  
-  const hashtag = '#' + cleanedWords[0].toUpperCase() + cleanedWords.slice(1).join('');
-  
+
+  // Prepend '#' and join the cleaned words with camel case structure
+  const hashtag = '#' + cleanedWords.map((word, index) => {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  }).join('');
+
+  if (hashtag.length > 140) {
+    return false; // Return false if hashtag is too long
+  }
+
   return hashtag;
 }
 
